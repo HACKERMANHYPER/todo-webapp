92bbdc72c7505a06f85980fe1e4c62f3188315e8
Steffen Krause <krause@be-on.de>
2025-11-28 14:05:17 +0100

    implement front and server

---
+++ resources/js/Pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Dashboard() {
+    return (
+        <div className="min-h-screen bg-white p-6">
+            <h1 className="text-3xl font-bold text-gray-900">Welcome to ToDo App</h1>
+            <p className="mt-4 text-gray-600">Inertia.js is working!</p>
+            <p className="mt-4 text-gray-600">Sven stinkt</p>
+        </div>
+    )
+}
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,30 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        // respond to all hosts
+        host: "0.0.0.0",
+        strictPort: true,
+        port: 5173,
+        cors: true,
+        hmr: {
+            // Force the Vite client to connect via SSL
+            // This will also force a "https://" URL in the public/hot file
+            protocol: "wss",
+            // The host where the Vite dev server can be accessed
+            // This will also force this host to be written to the public/hot file
+            host: "todo-webapp.ddev.site",
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
+        react(),
         tailwindcss(),
     ],
 });

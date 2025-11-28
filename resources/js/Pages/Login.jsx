910725bc461c75732a423f132dd9084a26070268
Steffen Krause <krause@be-on.de>
2025-11-28 14:25:14 +0100

    cleanup unused shit

--- resources/js/Pages/Login.jsx
+++ resources/js/Pages/Login.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Dashboard() {
+export default function Login() {
     return (
         <div className="min-h-screen bg-white p-6">
             <h1 className="text-3xl font-bold text-gray-900">Welcome to ToDo App</h1>
             <p className="mt-4 text-gray-600">Inertia.js is working!</p>
-            <p className="mt-4 text-gray-600">Sven stinkt</p>
+            <p className="mt-4 text-gray-600">Sven stinkt heftig</p>
         </div>
     )
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,4 @@
-import './bootstrap';
+// import './bootstrap';
 
 import React from 'react'
 import { createInertiaApp } from '@inertiajs/react'
